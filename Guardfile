c8542662dbd09415eda92a3cd48e1d11dbb2010d
Francisco Martins <franciscomxs@gmail.com>
2014-03-23 03:54:26 -0300

    Setup jekyll, sass, compass, neat and bourbon

--- Guardfile
+++ Guardfile
@@ -2,3 +2,5 @@ guard 'jekyll-plus', serve: true do
   watch /.*/
   ignore /^_site/
 end
+
+guard :compass
---
+++ config.rb
@@ -0,0 +1,10 @@
+require 'bourbon'
+require 'neat'
+
+http_path = '/'
+css_dir = 'stylesheets'
+sass_dir = 'sass'
+images_dir = 'images'
+javascripts_dir = 'javascripts'
+output_style = :compressed
+line_comments = false
