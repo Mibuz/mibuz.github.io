2e3e5caa6bd06e6fc650892711104f9624e60cc9
Bruno Fernando <bruno2fernando@gmail.com>
2014-03-23 19:13:38 -0300

    Organize assets

--- config.rb
+++ config.rb
@@ -2,9 +2,9 @@ require 'bourbon'
 require 'neat'
 
 http_path = '/'
-css_dir = 'stylesheets'
-sass_dir = 'sass'
+css_dir = 'assets/stylesheets'
+sass_dir = '_source/sass'
 images_dir = 'images'
-javascripts_dir = 'javascripts'
+javascripts_dir = 'assets/javascripts'
 output_style = :compressed
 line_comments = false
