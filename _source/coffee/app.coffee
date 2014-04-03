77efe2b8cb3665e0a1e4a8d64e3a6e38582a9467
Francisco Martins <franciscomxs@gmail.com>
2014-04-03 13:27:54 -0300

    Add ScrollTo function

--- _source/coffee/app.coffee
+++ _source/coffee/app.coffee
@@ -1,1 +1,14 @@
-console.info 'Mibuz'
+smoothScroll = (el, to, duration) ->
+  return  if duration < 0
+  difference = to - $(window).scrollTop()
+  perTick = difference / duration * 10
+  @scrollToTimerCache = setTimeout((->
+    unless isNaN(parseInt(perTick, 10))
+      window.scrollTo 0, $(window).scrollTop() + perTick
+      smoothScroll el, to, duration - 10
+    return
+  ).bind(this), 10)
+
+$('.scroll-to').on 'click', (e) ->
+  e.preventDefault()
+  smoothScroll $(window), $($(e.currentTarget).attr('href')).offset().top, 500
--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,4 +1,24 @@
 (function() {
-  console.info('Mibuz');
+  var smoothScroll;
+
+  smoothScroll = function(el, to, duration) {
+    var difference, perTick;
+    if (duration < 0) {
+      return;
+    }
+    difference = to - $(window).scrollTop();
+    perTick = difference / duration * 10;
+    return this.scrollToTimerCache = setTimeout((function() {
+      if (!isNaN(parseInt(perTick, 10))) {
+        window.scrollTo(0, $(window).scrollTop() + perTick);
+        smoothScroll(el, to, duration - 10);
+      }
+    }).bind(this), 10);
+  };
+
+  $('.scroll-to').on('click', function(e) {
+    e.preventDefault();
+    return smoothScroll($(window), $($(e.currentTarget).attr('href')).offset().top, 500);
+  });
 
 }).call(this);
