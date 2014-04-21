3e1df9b63e4f48a498970f4cdca5f49dfc670516
Bruno Fernando <bruno2fernando@gmail.com>
2014-04-21 15:05:16 -0300

    Improve style invalid form

--- _source/coffee/app.coffee
+++ _source/coffee/app.coffee
@@ -45,3 +45,25 @@ app.ParallaxScroll()
 $('.scroll-to').on 'click', (e) ->
   e.preventDefault()
   app.smoothScroll $(window), $($(@).attr('href')).offset().top
+
+# Validation Form
+$('#form_contact').isHappy(
+  fields:
+    '#Field1':
+        required: true,
+        message: 'Informe seu nome completo.'
+
+    '#Field2':
+      required: true,
+      message: 'Informe seu email correto.',
+      test: happy.email
+
+    '#Field4':
+        required: true,
+        message: 'Informe seu Skype ou telefone.'
+
+    '#Field5':
+        required: true,
+        message: 'Mensagem muito curta, nos explique melhor o seu projeto.'
+        minlength: 3
+)
--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -67,4 +67,27 @@
     return app.smoothScroll($(window), $($(this).attr('href')).offset().top);
   });
 
+  $('#form_contact').isHappy({
+    fields: {
+      '#Field1': {
+        required: true,
+        message: 'Informe seu nome completo.'
+      },
+      '#Field2': {
+        required: true,
+        message: 'Informe seu email correto.',
+        test: happy.email
+      },
+      '#Field4': {
+        required: true,
+        message: 'Informe seu Skype ou telefone.'
+      },
+      '#Field5': {
+        required: true,
+        message: 'Mensagem muito curta, nos explique melhor o seu projeto.',
+        minlength: 3
+      }
+    }
+  });
+
 }).call(this);
